683fb0065a267edf597208b376ac22b13eead875
Harshit Kumar Mishra <harshitkumarmishra5010@gmail.com>
2025-08-01 02:43:52 +0530

    Fix TS error: cast event.currentTarget for offsetWidth access

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useRef } from "react";
+import Image from "next/image";
 import {
   motion,
   useTransform,
@@ -33,13 +34,14 @@ export const AnimatedTooltip = ({
     springConfig,
   );
 
-  const handleMouseMove = (event: any) => {
+  const handleMouseMove = (event: React.MouseEvent<HTMLImageElement>) => {
     if (animationFrameRef.current) {
       cancelAnimationFrame(animationFrameRef.current);
     }
 
     animationFrameRef.current = requestAnimationFrame(() => {
-      const halfWidth = event.target.offsetWidth / 2;
+      const halfWidth = (event.currentTarget as HTMLImageElement).offsetWidth / 2;
+
       x.set(event.nativeEvent.offsetX - halfWidth);
     });
   };
@@ -84,7 +86,7 @@ export const AnimatedTooltip = ({
               </motion.div>
             )}
           </AnimatePresence>
-          <img
+          <Image
             onMouseMove={handleMouseMove}
             height={100}
             width={100}
